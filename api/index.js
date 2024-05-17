4593fd83cafe6505762082b01d24783d5b2f467b
Rizzz Maooo <rizqonmaulana05@gmail.com>
2024-05-17 08:18:22 +0700

    update

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from 'cors'
 import cookieParser from "cookie-parser";
+import bodyParser from 'body-parser'
 
 import router from "../src/routes/router.js";
 
@@ -8,6 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(cors())
 app.use(cookieParser())
+app.use(bodyParser())
 app.use(router)
 app.listen(9001, () => console.log("server runnn"));
 
